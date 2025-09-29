6339e5f77ee8529dd8dede9621fc111f38ef1f83
Zeynep <101248567+zeynepbass@users.noreply.github.com>
2025-09-29 16:22:33 +0300

    -

---
+++ firebase.example.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT_ID.appspot.com",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+  };
+  
